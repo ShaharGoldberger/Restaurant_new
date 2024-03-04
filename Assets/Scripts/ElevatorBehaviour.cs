cf27bfbccd798c1dda710689b75878d5fbc114ed
Shahar <shaharyehuda12@gmail.com>
2024-03-04 21:36:25 +0200

    Added elevator to move between scenes

--- Assets/Scripts/ElevatorBehaviour.cs
+++ Assets/Scripts/ElevatorBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElevatorBehaviout : MonoBehaviour
+public class ElevatorBehaviour : MonoBehaviour
 {
     Animator animator;
     // Start is called before the first frame update
